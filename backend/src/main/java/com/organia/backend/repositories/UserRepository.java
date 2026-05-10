6f5447a48593ddf78eec8db5180a866a96d9644f
Saru-Work <saruatwork@gmail.com>
2026-05-10 19:36:46 +0530

    Initialized

---
+++ backend/src/main/java/com/organia/backend/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.organia.backend.repositories;
+
+import com.organia.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
